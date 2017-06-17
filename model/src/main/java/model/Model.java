619d8d15bfd78ca2b6414a958bd6ad749a5029cf
Flo <florian.pfeifer@viacesi.fr>
2017-06-17 18:26:26 +0200

    Update and rename ModelFacade.java to Model.java

---
+++ model/src/main/java/model/Model.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.dao.ExampleDAO;
+
+public class Model implements IModel {
+
+    
+    public Model() {
+       
+    }
+
+    
+
+}
