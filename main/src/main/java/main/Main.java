930e9f6f6ee1a99866fa67d3aa2f746a5942507e
Flo <florian.pfeifer@viacesi.fr>
2017-06-17 18:25:36 +0200

    Update Main.java

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -2,32 +2,17 @@ package main;
 
 import java.sql.SQLException;
 
-import controller.ControllerFacade;
-import model.ModelFacade;
-import view.ViewFacade;
+import controller.Controller;
+import model.Model;
+import view.View;
 
-/**
- * <h1>The Class Main.</h1>
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
+
 public abstract class Main {
 
-    /**
-     * The main method.
-     *
-     * @param args
-     *            the arguments
-     */
+    
     public static void main(final String[] args) {
-        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
+        final Controller controller = new Controller(new View(), new Model());
 
-        try {
-            controller.start();
-        } catch (final SQLException exception) {
-            exception.printStackTrace();
-        }
     }
 
 }
